cb1f6780b66744b3ef47c89b63e65d1a8712947c
Vernon L Alatorre <admiralsf4@hotmail.com>
2018-10-03 22:33:53 -0500

    Week 7 Demo code example complete

---
+++ DMACC.CIS174.Web/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DMACC.CIS174.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
